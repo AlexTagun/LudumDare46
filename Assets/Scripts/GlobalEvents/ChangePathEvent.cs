d3f1b7ac1186255fad35e2efca00f94abc28f6eb
AlexKutsar <alekcandr_123qw@mail.ru>
2020-04-19 01:54:30 +0300

    add change path event

---
+++ Assets/Scripts/GlobalEvents/ChangePathEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePathEvent : GlobalEvent
+{
+    [SerializeField] private ReporterMovement _reporterMovement;
+    protected override void Execute()
+    {
+        gameObject.SetActive(true);
+        _reporterMovement.CurrentPath = gameObject.GetComponent<LineRenderer>();
+        _reporterMovement._isRunning = true;
+    }
+
+}
--- Assets/Scripts/ReporterMovement.cs
+++ Assets/Scripts/ReporterMovement.cs
@@ -6,14 +6,11 @@ using UnityEngine.AI;
 public class ReporterMovement : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent _navMeshAgent;
-    [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private LineRenderer _lineRenderer;
-    [SerializeField] private Transform firstStep;
 
-    [SerializeField] private float _speed;
+    public LineRenderer CurrentPath;
+    public bool _isRunning = false;
 
     private Vector3 nextWayPoint;
-    private Vector3 curNextWayPoint;
     private int curIndexWayPoint = 0;
     private void Awake()
     {
@@ -21,42 +18,30 @@ public class ReporterMovement : MonoBehaviour
 
     private void Start()
     {
-        //_navMeshAgent.SetDestination(firstStep.position);
     }
 
     void Update()
     {
-
-        MoveOnRoad ();
+        if (_isRunning)
+        {
+            MoveOnRoad();
+        }
     }
 
     public void MoveOnRoad ()
     {
-        /*if (transform.position == nextWayPoint)
-        {
-            //_navMeshAgent.destination =_lineRenderer.GetPosition(i);
-            if (curIndexWayPoint < _lineRenderer.positionCount-1)
-            curIndexWayPoint++;
-            nextWayPoint = _lineRenderer.GetPosition(curIndexWayPoint);
-            Debug.Log(nextWayPoint);
-            _navMeshAgent.SetDestination(nextWayPoint);
-            //transform.LookAt(nextWayPoint);
-        }
-        else
-        {
-            //transform.position = Vector3.MoveTowards(transform.position, nextWayPoint, Time.deltaTime * _speed);
-            //_rigidbody.MovePosition(nextWayPoint*Time.deltaTime);
-            //Debug.Log(nextWayPoint);
-            //_navMeshAgent.SetDestination(nextWayPoint);
-        } */
 
         if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
         {
-            if (curIndexWayPoint < _lineRenderer.positionCount - 1)
+            if (curIndexWayPoint < CurrentPath.positionCount - 1)
             {
                 curIndexWayPoint++;
-                nextWayPoint = _lineRenderer.GetPosition(curIndexWayPoint);
+                nextWayPoint = CurrentPath.GetPosition(curIndexWayPoint);
                 _navMeshAgent.SetDestination(nextWayPoint);
+            }
+            else
+            {
+                _isRunning = false;
             }
         }
     }
