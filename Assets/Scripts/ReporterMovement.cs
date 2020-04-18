c91e80b02e2cf4ca9b7d32bf585f0ca27beec4b5
AlexKutsar <alekcandr_123qw@mail.ru>
2020-04-18 23:04:55 +0300

    add move to point on line renderer

---
+++ Assets/Scripts/ReporterMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ReporterMovement : MonoBehaviour
+{
+    //[SerializeField] private NavMeshAgent _navMeshAgent;
+    [SerializeField] private LineRenderer _lineRenderer;
+
+    [SerializeField] private float _speed;
+
+    private Vector3 nextWayPoint;
+    private Vector3 curNextWayPoint;
+    private int curIndexWayPoint = 0;
+    private void Awake()
+    {
+        //_navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+
+        MoveOnRoad ();
+    }
+
+    public void MoveOnRoad ()
+    {
+        if (transform.position == nextWayPoint)
+        {
+            //_navMeshAgent.destination =_lineRenderer.GetPosition(i);
+            if (curIndexWayPoint < _lineRenderer.positionCount-1)
+            curIndexWayPoint++;
+            nextWayPoint = _lineRenderer.GetPosition(curIndexWayPoint);
+            transform.LookAt(nextWayPoint);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nextWayPoint, Time.deltaTime * _speed);
+        }
+    }
+
+
+}
