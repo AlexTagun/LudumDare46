9cac9c9aabe826a06a90117b1c4560c33feaa754
AlexKutsar <alekcandr_123qw@mail.ru>
2020-04-18 18:35:26 +0300

    rename scripts: CameramanMovement, CameramanInGameController

---
+++ Assets/Scripts/CameramanInGameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameramanInGameController : MonoBehaviour
+{
+    [SerializeField] private CameramanMovement _cameramanMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _cameramanMovement.MoveForward();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _cameramanMovement.MoveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _cameramanMovement.MoveBack();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _cameramanMovement.MoveRight();
+        }
+
+        _cameramanMovement.Rotate(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    }
+}
---
+++ Assets/Scripts/CameramanMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameramanMovement : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _speedRotation;
+
+    private float verticalSpeed;
+    private float horizontalSpeed;
+    // вращение камеры
+    private float mouseDeltaX;
+    private float mouseDeltaY;
+    private Quaternion startRotation;
+    private Quaternion verticalRotation;
+    private Quaternion horizontalRotarion;
+
+    private void Awake()
+    {
+        startRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MoveForward()
+    {
+        verticalSpeed = _speed * Time.deltaTime;
+        horizontalSpeed = 0f;
+        Move();
+    }
+
+    public void MoveBack()
+    {
+        verticalSpeed = (-1) * _speed * Time.deltaTime;
+        horizontalSpeed = 0f;
+        Move();
+    }
+    public void MoveLeft()
+    {
+        horizontalSpeed = (-1) * _speed * Time.deltaTime;
+        verticalSpeed = 0f;
+        Move();
+    }
+
+    public void MoveRight()
+    {
+        horizontalSpeed = _speed * Time.deltaTime;
+        verticalSpeed = 0f;
+        Move();
+    }
+
+    public void Rotate(float mouseDeltaX, float mouseDeltaY)
+    {
+        this.mouseDeltaX += mouseDeltaX * _speedRotation;
+        this.mouseDeltaY += mouseDeltaY * _speedRotation;
+        this.mouseDeltaY = Mathf.Clamp(this.mouseDeltaY, -60, 60);
+        this.gameObject.transform.rotation = Quaternion.AngleAxis(this.mouseDeltaX, Vector3.up);
+        verticalRotation = Quaternion.AngleAxis(this.mouseDeltaX, Vector3.up);
+        horizontalRotarion = Quaternion.AngleAxis(-this.mouseDeltaY, Vector3.right);
+        _camera.transform.rotation = startRotation * verticalRotation * horizontalRotarion;
+
+    }
+
+
+    private void Move()
+    {
+        transform.Translate(new Vector3(horizontalSpeed, 0, verticalSpeed));
+    }
+}
