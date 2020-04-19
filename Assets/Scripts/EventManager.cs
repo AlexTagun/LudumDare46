c3c87a66ec339910ffbf6c2c8a57a01b3fc12ac2
AlexTagun <alextagun@gmail.com>
2020-04-19 19:30:05 +0300

    add gameEnd action

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -6,14 +6,25 @@ using UnityEngine;
 public static class EventManager {
     public static Action<int> OnSecondTick;
 
-    public static Action OnCameraShake;
-
     public static void HandleOnSecondTick(int second) {
         OnSecondTick?.Invoke(second);
     }
 
-    public static void HandleOnCameraShake ()
-    {
+    public static Action OnCameraShake;
+
+    public static void HandleOnCameraShake() {
         OnCameraShake?.Invoke();
+    }
+
+    public enum EndGameType {
+        Win,
+        Die,
+        LowBattery
+    }
+
+    public static Action<EndGameType> OnEndGame;
+
+    public static void HandleOnEndGame(EndGameType type) {
+        OnEndGame?.Invoke(type);
     }
 }
