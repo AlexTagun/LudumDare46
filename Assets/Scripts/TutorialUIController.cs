f1f583e4e8d0d7d4387d562745da3f7ae1d7bc7a
AlexKutsar <alekcandr_123qw@mail.ru>
2020-04-20 00:47:42 +0300

    add UI tutorial

---
+++ Assets/Scripts/TutorialUIController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialUIController : MonoBehaviour
+{
+    [SerializeField] private GameObject _tutorialUIPanel;
+    [SerializeField] private Image _iconImage;
+    [SerializeField] private TextMeshPro _textPlaceUI;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ShowTutorialUIPanel()
+    {
+        _tutorialUIPanel.SetActive(true);
+    }
+    public void CloseTutorialUIPanel ()
+    {
+        _tutorialUIPanel.SetActive(false);
+    }
+    public void ChangeIcon (Sprite icon)
+    {
+        _iconImage.sprite = icon;
+    }
+    public void ChangeText (Text text)
+    {
+        _textPlaceUI.text = text.text;
+    }
+
+}
