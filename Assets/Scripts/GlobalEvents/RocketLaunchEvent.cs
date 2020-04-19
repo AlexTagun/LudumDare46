d2bbbd44adb6a46e8f170c0585290e34f13271ff
AlexKutsar <alekcandr_123qw@mail.ru>
2020-04-19 15:06:06 +0300

    add Rocket Launch Event

--- Assets/Scripts/GlobalEvents/RocketLaunchEvent.cs
+++ Assets/Scripts/GlobalEvents/RocketLaunchEvent.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class RocketLaunchEvent : GlobalEvent {
+
+    [SerializeField] private float _duraction;
+    [SerializeField] private float _endPosition;
+
     protected override void Execute() {
         Debug.Log("Rocket Launched");
+        transform.DOMoveY(_endPosition, _duraction).OnComplete(() => { Destroy(gameObject); });
     }
 }
