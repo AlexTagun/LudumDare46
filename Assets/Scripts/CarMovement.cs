87e2e751367c0bef789ed5a0ffd905fb5d1cb9a8
AlexKutsar <alekcandr_123qw@mail.ru>
2020-04-19 22:43:21 +0300

    add moving car

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private int timeToChangePoint;
+
+    [SerializeField] private Transform[] pointsToMove;
+
+    private NavMeshAgent _navMeshAgent;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (gameObject.GetComponent<NavMeshAgent>() != null)
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+        else
+        {
+            gameObject.AddComponent<NavMeshAgent>();
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        };
+
+        EventManager.OnSecondTick += GoToRandomPoint;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToRandomPoint (int sec)
+    {
+        if (sec % timeToChangePoint == 0)
+        {
+            int randomIndex = Random.Range(0, pointsToMove.Length);
+            _navMeshAgent.SetDestination(pointsToMove[randomIndex].transform.position);
+            Debug.Log(pointsToMove[randomIndex].transform.position);
+        }
+    }
+}
--- Assets/Scripts/PeopleMovement.cs
+++ Assets/Scripts/PeopleMovement.cs
@@ -38,4 +38,6 @@ public class PeopleMovement : MonoBehaviour
             _navMeshAgent.SetDestination(randomPosition);
         }
     }
+
+    
 }
