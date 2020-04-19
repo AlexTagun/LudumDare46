158c563432942e26c471faa2cf0512c281099dd1
AlexTagun <alextagun@gmail.com>
2020-04-19 12:26:13 +0300

    add Tree_pine_animated

---
+++ Assets/Scripts/AnimationStarter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AnimationStarter : MonoBehaviour {
+    [SerializeField] private Animator _animator;
+    [SerializeField] private string _animationName;
+    
+    void Start() {
+        _animator.Play(_animationName);
+    }
+}
