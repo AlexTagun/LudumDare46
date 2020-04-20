cf333f2742bd4b683179c7576994608724932880
AlexTagun <alextagun@gmail.com>
2020-04-20 19:35:49 +0300

    add reporter animation

---
+++ Assets/Scripts/ReporterAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReporterAnimator : MonoBehaviour {
+    [SerializeField] private Animator _animator;
+    
+    public void Run() {
+        _animator.Play("Reporter_run_cure");
+    }
+
+    public void Talk() {
+        _animator.Play("Reporter_talking_cure");
+    }
+
+    public void Jump() {
+        _animator.Play("Reporter_talking_cure");
+    }
+}
