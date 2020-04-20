6867a7ea0f5328de27b0afe77ac24c975cfc1b43
AlexKutsar <alekcandr_123qw@mail.ru>
2020-04-20 22:43:17 +0300

    add fall whale

---
+++ Assets/Scripts/GlobalEvents/FallWhaleEvent.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallWhaleEvent : GlobalEvent
+{
+    [SerializeField] private Transform _pointToFallWhale;
+    [SerializeField] private float _timeFalling;
+    [SerializeField] private float _speedRotation;
+
+    private void Start()
+    {
+    }
+    protected override void Execute()
+    {
+        FallingWhale();
+    }
+
+    private void FallingWhale ()
+    {
+        transform.DOMove(_pointToFallWhale.position, _timeFalling);
+        transform.DORotate(Vector3.up * _speedRotation, _timeFalling);
+    }
+
+}
