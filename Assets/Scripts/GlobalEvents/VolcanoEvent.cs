6bfee0ec08509da10ed1c4349a265a742a3c25f6
AlexTagun <alextagun@gmail.com>
2020-04-21 00:28:01 +0300

    add volcano anim

--- Assets/Scripts/GlobalEvents/VolcanoEvent.cs
+++ Assets/Scripts/GlobalEvents/VolcanoEvent.cs
@@ -9,6 +9,7 @@ public class VolcanoEvent : GlobalEvent
     [SerializeField] private float _positionOnGround;
     [SerializeField] private float _preparationTimeToEruption;
     [SerializeField] private ParticleSystem _particleSystemEruptionVolcano;
+    [SerializeField] private Animator _animator;
 
     protected override void Execute()
     {
@@ -19,6 +20,7 @@ public class VolcanoEvent : GlobalEvent
     private void Awake()
     {
        _particleSystemEruptionVolcano.gameObject.SetActive(false);
+       _animator.enabled = false;
     }
 
 
@@ -28,5 +30,7 @@ public class VolcanoEvent : GlobalEvent
         yield return new WaitForSeconds(_preparationTimeToEruption + _timeRiseFromUnderground);
         _particleSystemEruptionVolcano.gameObject.SetActive(true);
         _particleSystemEruptionVolcano.Play();
+        _animator.enabled = true;
+        _animator.Play("volcano");
     }
 }
